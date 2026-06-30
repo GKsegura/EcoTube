74e62f0bf384e55f84677518c9e40f5274fb2d55
José Segura <jose.segura.ms@gmail.com>
2026-06-29 22:34:38 -0300

    fix: corrigir contraste, hover e transição do tema escuro

--- includes/theme_toggle.php
+++ includes/theme_toggle.php
@@ -5,6 +5,8 @@
     }
 })();
 function toggleTheme() {
+    document.body.classList.add('theme-transitioning');
+
     if (document.body.getAttribute('data-theme') === 'dark') {
         document.body.removeAttribute('data-theme');
         localStorage.setItem('theme', 'light');
@@ -12,5 +14,9 @@ function toggleTheme() {
         document.body.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
     }
+
+    setTimeout(function() {
+        document.body.classList.remove('theme-transitioning');
+    }, 400);
 }
 </script>
